fe902f211da78e3da3c5ca27c7683fdb594a5e04
TheThinkingFool <91555895+TheThinkingFool@users.noreply.github.com>
2023-07-20 14:18:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-"""
-Guides:
-1. Change Paths by yourself
-2. Do not contain other files or folders in the imgDirPath, only contains 36 images
-3. name images in the format of "DD-MM (x).jpg" or "DD-MM.jpg"
-4. use the sample document to generate
-5. change the Month in the header by yourself
-6. ignore weak warnings in PyCharm.
-7. Please make sure your installed docx python with pip in windows console.
-
-This program is coded by Li Chung Ho, John
-input:  a sample Word file, 36 images in a folder (named in format of (3))
-        = docPath           = imgDirPath
-output: = outputPath, can change name
-"""
 import random
 
 from docx import Document
